b1c7f9e502f046bc92c4d6951979e6ec7fab984a
chandru <chandru@xtrafin.net>
2011-01-10 17:40:26 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cerp::Application.config.secret_token = 'c2ea9991d8aa548d629e26713a47ecf4f6797b2bef724f4a87631dd3beb76c29a0202f1a7ed8ae1cc3afa20a3cd72b060aad7b80fba986e5cf9a0d1136f17c3f'
