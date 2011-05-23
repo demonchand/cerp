857c27e36874ecc334e8971cc6d99e6bc9aaefad
chandru <chandru@xtrafin.net>
2011-05-23 10:04:41 +0530

    remove the unwanted routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Cerp::Application.routes.draw do
 
   devise_for :users
   
-  get "home/index"
+#  get "home/index"
   root :to => "home#index"
 
   # The priority is based upon order of creation:
@@ -65,5 +65,5 @@ Cerp::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-   match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id(.:format)))'
 end
